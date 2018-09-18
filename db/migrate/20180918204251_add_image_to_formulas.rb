b957982ecb9f2205271181c5205b3b4d537833fa
Sabrina Robinson <sabrinarob@gmail.com>
2018-09-18 15:32:01 -0600

    Removes image column because image is displayed as null

--- db/migrate/20180918204251_add_image_to_formulas.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToFormulas < ActiveRecord::Migration[5.2]
-  def change
-    add_column :formulas, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_204251) do
+ActiveRecord::Schema.define(version: 2018_09_12_015906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2018_09_18_204251) do
     t.text "ingredients"
     t.string "hcpc"
     t.string "restrictions"
-    t.string "image"
   end
 
   create_table "nutritional_contents", force: :cascade do |t|
